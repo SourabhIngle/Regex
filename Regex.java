c2f5352173ce9c7dac91ce3335a094996e2a35fd
SourabhIngle <100rabhingle@gmail.com>
2023-06-15 13:00:16 +0530

    Validate Last Name

--- Regex.java
+++ Regex.java
@@ -3,17 +3,17 @@ package com.regex;
 import java.util.Scanner;
 
 public class Regex {
-    static String VALID_PATTERN = "^[A-Z][a-z]{2,}$";
+    static String VALID_LAST_NAME = "^[A-Z][a-z]{2,}$";
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter First Name :");
+        System.out.println("Enter Last Name :");
         String name = sc.next();
-        boolean valid = name.matches(VALID_PATTERN);
+        boolean valid = name.matches(VALID_LAST_NAME);
         if (valid) {
-            System.out.println("Valid First Name! ");
+            System.out.println("Valid Last Name! ");
         } else {
-            System.out.println("Not valid first name. First letter should " +
+            System.out.println("Not valid Last name. First letter should " +
                     "Capital rest all small and more then two letters. ");
         }
     }
