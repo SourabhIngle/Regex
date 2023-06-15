98de9eb973afeef9370ef992b618d3a611279168
SourabhIngle <100rabhingle@gmail.com>
2023-06-15 13:11:47 +0530

    Valid Password Rule 3

--- Regex.java
+++ Regex.java
@@ -3,15 +3,15 @@ package com.regex;
 import java.util.Scanner;
 
 public class Regex {
-    static String VALID_PASSWORD_PATTERN ="([A-Za-z]{8})$";
+    static String VALID_PATTERN = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]{7,}).+$";
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Password : ");
+        System.out.println("Enter Password :");
         String name = sc.next();
-        boolean valid = name.matches(VALID_PASSWORD_PATTERN);
+        boolean valid = name.matches(VALID_PATTERN);
         if (valid) {
-            System.out.println("Valid Password Rule 2. ");
+            System.out.println("Valid Password Rule 3.");
         } else {
             System.out.println("Not valid rule for password. ");
         }
