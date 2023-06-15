10013ea5a75354be8daa5f6cb093a7e5518ea8d6
SourabhIngle <100rabhingle@gmail.com>
2023-06-15 12:58:03 +0530

    Validate First Name

--- Regex.java
+++ Regex.java
@@ -1,7 +1,20 @@
 package com.regex;
 
+import java.util.Scanner;
+
 public class Regex {
+    static String VALID_PATTERN = "^[A-Z][a-z]{2,}$";
+
     public static void main(String[] args) {
-        System.out.println("Welcome to regex (Regular Expression)");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Name :");
+        String name = sc.next();
+        boolean valid = name.matches(VALID_PATTERN);
+        if (valid) {
+            System.out.println("Valid First Name! ");
+        } else {
+            System.out.println("Not valid first name. First letter should " +
+                    "Capital rest all small and more then two letters. ");
+        }
     }
 }
